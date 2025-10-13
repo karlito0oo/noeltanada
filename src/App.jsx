413e47d5c5528cd753c5eb570bd74f1bda5e8235
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2025-10-14 04:31:25 +0800

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import HeroSection from "./components/HeroSection";
-import AboutNoel from "./components/AboutNoel";
-import FeaturedCollectionSection from "./components/FeaturedCollectionSection";
-import OurStorySection from "./components/OurStorySection";
-import FeaturedArticlesSection from "./components/FeaturedArticlesSection";
-import FeaturedVideosSection from "./components/FeaturedVideosSection";
-import FooterSection from "./components/FooterSection";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      <HeroSection />
-      <AboutNoel />
-      <FeaturedCollectionSection />
-      <OurStorySection />
-      <FeaturedArticlesSection />
-      <FeaturedVideosSection />
-      <FooterSection />
+      <Homepage />
     </>
   );
 }
